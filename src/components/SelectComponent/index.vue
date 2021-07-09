03841a97eacadbacdcd85ca07cba70173ae21001
Marcus Lucena <marcus-lucena@outlook.com>
2021-07-09 15:25:46 -0300

    Componente de seleção

---
+++ src/components/SelectComponent/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="select-component">
+    <select
+      v-model="valueSelected"
+      @select="$emit('select', $event.target.value)"
+    >
+      <option v-for="item in values" @key="item.value" :value="item.value">
+        {{ item.label }}
+      </option>
+    </select>
+    <Validate />
+  </div>
+</template>
+
+<script>
+import Validate from "@/components/Validate";
+
+export default {
+  name: "SelectComponent",
+  components: {
+    Validate,
+  },
+  props: {
+    values: {
+      default: [],
+    },
+  },
+  data() {
+    return {
+      valueSelected: "",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
