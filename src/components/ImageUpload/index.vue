13bf6f452a95723e88e8a2534345f68cad689256
Marcus Lucena <marcus-lucena@outlook.com>
2021-07-09 14:21:14 -0300

    Componente de upaload de imagens

---
+++ src/components/ImageUpload/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="image-upload">
+    <label>{{ label }}</label>
+    <input type="file" accept="image/" @change="upload" draggable="true" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ImageUpload",
+  props: {
+    label: {
+      type: String,
+      default: "Add image",
+    },
+  },
+  data() {
+    return {
+      preview: null,
+      image: null,
+    };
+  },
+  methods: {
+    upload(e) {
+      const input = e.target;
+      if (input.files) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          this.preview = e.target.result;
+        };
+        this.image = input.files[0];
+        reader.readAsDataURL(input.files[0]);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,20 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
-    <InputText />
+    <ImageUpload />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
-import InputText from "@/components/InputText";
+import ImageUpload from "@/components/ImageUpload";
 
 export default {
   name: "Home",
   components: {
     HelloWorld,
-    InputText,
+    ImageUpload,
   },
 };
 </script>
